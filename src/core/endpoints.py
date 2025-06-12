41643bce8621b1ef7c953b5ea86d7f8b032b0754
DaniilSever <jhisjfiohig14@mail.ru>
2025-06-13 04:55:09 +0700

    add docker && docker-compose, add endpoints

---
+++ src/core/endpoints.py
@@ -0,0 +1,64 @@
+class _EndpointsAUTH:
+    """Эндпоинты Auth сервиса"""
+
+    AUTH_HEALTHCHECK = "/api/auth/healthcheck"
+    AUTH_SIGNUP_EMAIL = "/api/auth/signup/email"
+    AUTH_SIGNIN_EMAIL = "/api/auth/signin/email"
+    AUTH_CONFIRM_EMAIL = "/api/auth/confirm/email"
+    AUTH_REFRESH_TOKEN = "/api/auth/refresh/token"
+    AUTH_REVOKE_TOKEN = "/api/auth/revoke/token"
+
+
+class _EndpointsACCOUNT:
+    """Эндпоинты Account сервиса"""
+
+    ACCOUNT_HEALTHCHECK = "/api/account/healthcheck"
+    ACCOUNT_CURRENT = "/api/account/current"
+    ACCOUNT_GET_BY_ID = "/api/acccount/{acc_id}"
+    ACCOUNT_GET_WARNINGS = "/api/account/warning"
+    ACCOUNT_GET_WARNING_BY_ID = "/api/account/warning/{warn_id}"
+    ACCOUNT_GET_ADS = "/api/account/ads"
+
+
+class _EndpointsADS:
+    """Эндпоинты ADS сервиса"""
+
+    ADS_HEALTHCHECK = "/api/ads/healthcheck"
+
+    # --- Действия с объявлениями
+    ADS_CREATE = "/api/ads/create"
+    ADS_CHANGE = "/api/ads/change/{ads_id}"
+    ADS_DELETE = "/api/ads/delete/{ads_id}"
+    ADS_GET_ALL = "/api/ads"
+    ADS_GET_BY_ID = "/api/ads/{ads_id}"
+    ADS_ADD_COMPLAINT = "/api/ads/{ads_id}/complaint"
+
+    # --- Действия с комментариями
+    ADS_ADD_COMMENTARY = "/api/ads/{ads_id}/commentary"
+    ADS_GET_COMMENTATIES = "/api/ads/{ads_id}"
+    ADS_CHANGE_COMMENTARY = "/api/ads/{ads_id}/{comm_id}"
+    ADS_DELETE_COMMENTARY = "/api/ads/{ads_id}/{comm_id}"
+    ADS_ADD_COMPLAINT_COMMENTARY = "/api/ads/{ads_id}/{comm_id}/complaint"
+
+
+class _EndpointsADM:
+    """Админские эндпоинты"""
+
+    ADM_HEALTHCHECK = "/api/adm/healthcheck"
+    ADM_DELETE_ADS = "/api/adm/delete/ads/{ads_id}"
+    ADM_DELETE_COMMENTARY = "/api/adm/delete/commentary/{comm_id}"
+    ADM_BAN_ACCOUNT = "/api/adm/ban/{acc_id}"
+    ADM_WARNING_ACCOUNT = "/api/adm/warn/{acc_id}"
+    ADM_UNBAN_ACCOUNT = "/api/adm/unban/{acc_id}"
+    ADM_SET_ADM = "/api/adm/setadm/{acc_id}"
+    ADM_GET_COMPLAINTS = "/api/adm/complaint"
+    ADM_GET_COMPLAINT_BY_ID = "/api/adm/complaint/{comp_id}"
+
+
+class Endpoints(
+    _EndpointsAUTH,
+    _EndpointsACCOUNT,
+    _EndpointsADS,
+    _EndpointsADM,
+):
+    """Глобальные класс эндпоинтов"""
